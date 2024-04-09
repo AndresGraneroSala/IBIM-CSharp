fc44f94974bf603234fbe4c6d49879c2d5855350
AndresGS <andresnn21@gmail.com>
2024-04-09 12:04:15 +0200

    mi solucion v46

--- Ej46.cs
+++ Ej46.cs
@@ -2,6 +2,24 @@
 {
 
 	// Video 46
+
+
+	/*
+	 ·Creacion de tres clases
+		-Avión
+		-Vehículo
+		-Coche
+	
+	·Métodos coumnes a las tres clases
+		-Arrancar motor
+		-Parar motor
+
+	·Método virtual
+		-Conducir
+
+	 
+	 */
+
 	class Ej46
 	{
 
@@ -9,5 +27,52 @@
 		{
             Console.WriteLine("a");
         }
+
+
+		class Vehiculo
+		{
+			private double velocidad = 1;
+
+			public Vehiculo(double velocidad)
+			{
+				this.velocidad = velocidad;
+			}
+
+			public void Arrancar()
+			{
+                Console.WriteLine("arrancamos motores");
+            }
+
+			public void Parar()
+			{
+                Console.WriteLine("paramos motores");
+            }
+
+			public virtual void Conducir()
+			{
+                Console.WriteLine("conduciendo");
+            }
+
+		}
+
+		class Avion: Vehiculo 
+		{
+			public override void Conducir()
+			{
+                Console.WriteLine("Conducir por el aire");
+            }
+		}
+
+		class Coche: Vehiculo
+		{
+			public override void Conducir()
+			{
+				Console.WriteLine("Conducir por la carretera");
+			}
+		}
+
+
+
+
 	}
 }
